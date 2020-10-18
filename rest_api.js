570663fd481aafebaa4dd28e431c285beb0434a4
Nazli Beitollahpour <nazli.beitollahpour@umontreal.ca>
2020-10-17 22:37:43 -0400

    POST /counters

--- rest_api.js
+++ rest_api.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 let counters = new Map();
+app.use(bodyParser.json());
+
 app.get('/counters/',(req,res)=>{
+  res.send(counters);
+});
+
+app.post('/counters',(req,res)=>{
+  for (const [key, value] of Object.entries(req.body)) {
+    counters[key] = value;
+  }
   res.send(counters);
 });
 
