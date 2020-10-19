14fdc2f9186e3e9265e1519432f34511d55f1ea8
Nazli Beitollahpour <nazli.beitollahpour@umontreal.ca>
2020-10-18 20:15:28 -0400

    PUT /counters/:counter

--- rest_api.js
+++ rest_api.js
@@ -3,17 +3,35 @@ const bodyParser = require('body-parser');
 const app = express();
 
 let counters = new Map();
+
+function map_to_object(map) {
+  const out = Object.create(null)
+  map.forEach((value, key) => {
+      out[key] = value
+  })
+  return out
+}
 app.use(bodyParser.json());
 
 app.get('/counters/',(req,res)=>{
-  res.send(counters);
+  res.send(map_to_object(counters));
 });
 
 app.post('/counters',(req,res)=>{
   for (const [key, value] of Object.entries(req.body)) {
-    counters[key] = value;
+    counters.set(key, value);
   }
-  res.send(counters);
+  res.send(map_to_object(counters));
+});
+
+app.put('/counters/:counter',(req, res)=>{
+  let counter_name = req.params.counter;
+  if (! counters.has(counter_name)) {
+    res.status(404).send();
+    return;
+  }
+  counters.set(counter_name,  counters.get(counter_name)+1);
+  res.send(map_to_object(counters));
 });
 
 app.listen('3000');
